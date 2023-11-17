9ac507c49bae7a3daa952a8a5cb3ca88976e79d0
felipehmazotti <126676451+felipehmazotti@users.noreply.github.com>
2023-11-17 11:58:04 -0300

    commit

--- crm/src/components/menu/menu.jsx
+++ crm/src/components/menu/menu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import logo from"../../assets/logo.png";
+import logo from"../../assets/logo.jpeg";
 import "./menu.css"
 
 function Menu(props){
